8c72bec21603c2dbea88fe3f4f8ad967056a466a
bboynam99 <30984515+bboynam99@users.noreply.github.com>
2021-04-08 17:30:49 +0700

    update

---
+++ static/js/8.850da130.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsalt-frontend-amm"]=this["webpackJsonpsalt-frontend-amm"]||[]).push([[8],{619:function(n,t){},620:function(n,t){},629:function(n,t){},630:function(n,t){},650:function(n,t){},653:function(n,t){},654:function(n,t){},659:function(n,t){},663:function(n,t){},712:function(n,t){}}]);
+//# sourceMappingURL=8.850da130.chunk.js.map
---
+++ static/js/9.0cbe29b6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsalt-frontend-amm"]=this["webpackJsonpsalt-frontend-amm"]||[]).push([[9],{619:function(n,t){},620:function(n,t){},629:function(n,t){},630:function(n,t){},650:function(n,t){},653:function(n,t){},654:function(n,t){},659:function(n,t){},663:function(n,t){},965:function(n,t){}}]);
+//# sourceMappingURL=9.0cbe29b6.chunk.js.map
